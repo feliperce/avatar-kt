d62c45a5ab64544a720eb7cae51b1ba27120a835
Felipe Celestino <felipe.rce@gmail.com>
2026-04-01 21:39:07 -0300

    Add Kotlin Multiplatform setup to library module and configure WASM target

---
+++ library/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release", "debug")
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "AvatarKt"
+            isStatic = true
+        }
+    }
+    
+    jvm()
+    
+    js {
+        browser()
+    }
+    
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.ui)
+            implementation(compose.material3)
+        }
+    }
+}
+
+android {
+    namespace = "io.github.feliperce.avatar"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
